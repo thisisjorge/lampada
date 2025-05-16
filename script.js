7c71c0abf5f573fc35f48ee0c87bb1a8f59d47f4
Jorge Tavares <136516363+thisisjorge@users.noreply.github.com>
2025-05-15 21:33:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isLightOn) {
             // Desliga a luz
             body.style.background = 'radial-gradient(circle, white 8%, black 100%)';
-            lamp.src = 'assets/lamp_off.png';
+            lamp.src = 'lamp_off.png';
             lamp.alt = 'Lâmpada apagada';
         } else {
             // Liga a luz
             body.style.background = 'radial-gradient(circle, white 8%, white 100%)';
-            lamp.src = 'assets/lamp_on.png';
+            lamp.src = 'lamp_on.png';
             lamp.alt = 'Lâmpada acesa';
         }
         
